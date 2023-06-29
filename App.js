6520ff2fdc925fc8be2492c3d9f4e16687f6b45e
Pablo Villedae <pablov.e12345@gmail.com>
2023-06-29 15:14:06 -0700

    add alert

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button } from 'react-native';
 import { StyleSheet, Text, View,TouchableWithoutFeedback,TouchableOpacity,TouchableHighlight , SafeAreaView, Image } from 'react-native';
 
 export default function App() {
 
   const handlePress = () => console.log("text pressed"); 
-  const tap = () => console.log(" its pressed")
+  const tap = () => alert('ALERT')
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app! </Text>
-      <TouchableHighlight onPress={tap}>
-        <Image
-        source={{
-        width: 200, 
-        height: 300,
-        uri: 'https://picsum.photos/200/300'}} />
-      </TouchableHighlight>
+      <Button 
+      title='Click me' 
+      color="blue"
+      onPress={tap}
+      />
     </SafeAreaView>
   );
 }
