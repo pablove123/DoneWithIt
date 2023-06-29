12c04c3b0932c9ebb0d204ad8888815069e3cc77
Pablo Villedae <pablov.e12345@gmail.com>
2023-06-29 15:07:30 -0700

    first committed

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableWithoutFeedback, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("text pressed"); 
+  const tap = () => console.log(" its pressed")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app! </Text>
+      <TouchableWithoutFeedback onPress={tap}>
+        <Image
+        source={{
+        width: 200, 
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}} />
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center', 
+    alignItems: 'center'
   },
 });
